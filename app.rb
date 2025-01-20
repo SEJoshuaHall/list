d0e0ca713ce354a72f3ad03977bf736929274be7
Joshua Michael Hall <SEJoshuaHall@gmail.com>
2025-01-20 14:39:44 -0600

    homework 1/20/25 part 2

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require "sinatra/reloader"
 require "tilt/erubi"
 
 get "/" do
-  @contents = Dir.glob("public/*")
+  @contents = Dir.glob("public/*").sort
+  @contents.reverse! if params[:order] == 'descending'
   erb :home
 end
